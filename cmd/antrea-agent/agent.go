8a0730f4d23309fd214dcdfe67c68083d02782b9
DeshDeepakKant <deshdeepakkant@gmail.com>
2025-02-07 15:06:40 +0530

    Remove .git directory and add it to .gitignore

--- cmd/antrea-agent/agent.go
+++ cmd/antrea-agent/agent.go
@@ -107,6 +107,7 @@ var ipv4Localhost = net.ParseIP("127.0.0.1")
 
 // run starts Antrea agent with the given options and waits for termination signal.
 func run(o *Options) error {
+	klog.InfoS("LFX Mentorship 2025_01 test task #6975 log", "Github username", "DeshDeepakKant", "Pod name ", env.GetPodName())
 	klog.InfoS("Starting Antrea Agent", "version", version.GetFullVersion())
 
 	// Create K8s Clientset, CRD Clientset, Multicluster CRD Clientset and SharedInformerFactory for the given config.
